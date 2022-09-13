77e77eb268741b086166f24bf8b1500cbd30ccbe
erieevoicon <thanhtung161094@gmail.com>
2022-09-13 17:39:33 +0700

    ch8

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Local apps
     path("", include("pages.urls")), 
-    path("accounts/", include("accounts.urls")), 
     # User management
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
